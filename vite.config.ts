8403bc612e75815071ae3498f9a0b312b1337f62
Git-rule-breaker <mayanksingh19218@gmail.com>
2025-04-12 23:38:06 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { someFunction } from 'lovable-tagger';
+
+export default {
+  plugins: [
+    // Your other plugins
+  ]
+};
+
